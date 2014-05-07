19ab4326272d331de2846bec7f5dfba28d7f4e88
Cheddar2012 <cheddar1@outlook.com>
2014-05-06 22:51:08 -0700

    Login database up and running.

--- SwarchServer/SwarchServer/Program.cs
+++ SwarchServer/SwarchServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Timers;
 using System.Collections.Generic;
 using System.Text;
@@ -13,6 +14,8 @@ namespace SwarchServer
 {
     class Program
     {
+        public static SQLiteDatabase db;
+
         public static bool playing = false;
 
         public static PlayerSocket[] getPlayerSockets()
@@ -25,6 +28,8 @@ namespace SwarchServer
 
         public static void Main()
         {
+            // Start network connection
+
             listener = new TcpListener(4645);
             listener.Start();
 
@@ -53,6 +58,7 @@ namespace SwarchServer
                 Console.WriteLine("Error during connection.");
             }
 
+            Program.db = new SQLiteDatabase();
             playerSockets[0].writer.WriteLine("connect&start");
             Console.WriteLine("Player connected. Begin playing.");
             playing = true;
@@ -69,6 +75,8 @@ namespace SwarchServer
         int client;
         string data;
         string tempData;
+        string username;
+        string password;
 
         public ReadThread(int clientNumber)
         {
@@ -85,6 +93,56 @@ namespace SwarchServer
                     PlayerSocket[] sockets = Program.getPlayerSockets();
                     data = sockets[client - 1].reader.ReadLine();
                     tempData = data.Split('&')[0];
+
+                    if (tempData == "logins")
+                    {
+                        username = data.Split('&')[1];
+                        password = data.Split('&')[2];
+
+                        
+                        Dictionary<String, String> insertData = new Dictionary<String, String>();
+                        insertData.Add("USERNAME", username);
+                        insertData.Add("PASSWORD", password);
+                        try
+                        {
+                            bool entered = false; // Whether or not the username exists in the database
+                            Console.WriteLine("Inserting user data.");
+
+                            DataTable user;
+                            String query = "select USERNAME \"Username\", PASSWORD \"Password\"";
+                            query += "from USERS";
+                            user = Program.db.GetDataTable(query);
+                            foreach (DataRow r in user.Rows)
+                            {
+                                if (r["Username"].ToString() == username)
+                                {
+                                    Console.WriteLine("Found a username match");
+                                    if (r["Password"].ToString() == password)
+                                    {
+                                        Console.WriteLine("Found a password match. Send data to client.");
+                                        Program.getPlayerSockets()[0].writer.WriteLine("login&" + username);
+                                        entered = true;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Password didn't match. Send fail to client.");
+                                        Program.getPlayerSockets()[0].writer.WriteLine("login&" + "fail");
+                                        entered = true;
+                                    }
+                                }
+                            }
+                            if (!entered)
+                            {
+                                Console.WriteLine("No username match. Create a new user. Send data to client.");
+                                Program.db.Insert("USERS", insertData);
+                                Program.getPlayerSockets()[0].writer.WriteLine("login&" + username + "&" + password);
+                            }
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Failed to insert user");
+                        }
+                    }
                 }
             }
             catch
